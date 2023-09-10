f56e3ee4cc1dec2c1275d2e3039301ffd6d38b49
KonnyGuo <guokonny@gmail.com>
2023-09-10 19:27:14 -0400

    route

---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+//authentication routes
+//need express for router
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+
+//@desc auth with google
+//@router GET / /auth/google
+//routes tells server where to send and get data from 
+router.get("/", passport.authenticate('google',{scope: ['profile']}))
+
+
+//@desc google auth callback
+//@router GET /auth/google/callback
+router.get("/google/callback", passport.authenticate('google', {
+    failureRedirect: "/"
+}), (req,res) => {
+    //login successful go dashboad else fail go "/"
+    res.redirect("/dashboard")
+})
+
+module.exports = router
