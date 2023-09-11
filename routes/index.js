23a2ea8a8f30ec248ccd24ef91342f01a3512443
KonnyGuo <guokonny@gmail.com>
2023-09-10 22:09:17 -0400

    route protection

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 //need express for router
 const express = require('express')
+//routes tells server where to send and get data from 
 const router = express.Router()
+//destructuring allows us to bring in both of these item at same time and perform action
+const {ensureAuth, ensureGuest} = require('../middleware/auth')
 
 //@desc login/landing page
 //@router GET / 
-//routes tells server where to send and get data from 
-router.get("/", (req, res) => {
+//reusable protection for routes in app
+router.get("/", ensureGuest, (req, res) => {
     //By setting layout to false, Express will render "login.hbs" without trying to use the "main.hbs" layout file.
     //templating engines like Handlebars or EJS used with Express.js, "layout" refers to both a concept and, often, a specific template. layout is a special kind of folder for common structures
     res.render("login", {
@@ -17,7 +20,7 @@ router.get("/", (req, res) => {
 
 //@desc dashboard page
 //@router GET /dashboard
-router.get("/dashboard", (req, res) => {
+router.get("/dashboard", ensureAuth, (req, res) => {
     res.render("dashboard")
 })
 
